60655cd53a01d3fb9db2204de41b815d184e3138
melda bolat <121611832+meldabolat@users.noreply.github.com>
2024-07-22 21:05:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Space } from "antd";
+import "./App.css";
+import AppHeader from "./components/AppHeader";
+import SideMenu from "./components/SideMenu";
+import PageContent from "./components/PageContent";
+import AppFooter from "./components/AppFooter";
+import React from 'react';
+function App(){
+  return ( <div className="App">
+<AppHeader></AppHeader>
+<Space className="SideMenuPAgeContent">
+  <SideMenu></SideMenu>
+  <PageContent></PageContent>
+</Space>
+<AppFooter></AppFooter>
+  </div>
+  )
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+};
 
 export default App;
---
+++ src/components/PageContent/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppRoutes from '../AppRoutes';
+
+function PageContent(){
+    return <div className="PageContent">
+      <AppRoutes>
+        
+      </AppRoutes>
+    </div>;
+  }
+  export default PageContent;
